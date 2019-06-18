a1d7a498e5eea85da993b894055a6a113e6ed891
qshine <qjianxun0@gmail.com>
2019-06-19 01:32:10 +0800

    swagger

--- handler/user/create.go
+++ handler/user/create.go
@@ -10,7 +10,14 @@ import (
 	"go-apiserver/util"
 )
 
-// 新创建用户
+// @Summary Add new user to the database
+// @Description Add a new user
+// @Tags user
+// @Accept  json
+// @Produce  json
+// @Param user body user.CreateRequest true "Create a new user"
+// @Success 200 {object} user.CreateResponse "{"code":0,"message":"OK","data":{"username":"kong"}}"
+// @Router /user [post]
 func Create(c *gin.Context) {
 	log.Info("User create function called", lager.Data{"X-Request-Id": util.GetReqID(c)})
 	var r CreateRequest
